11f44a795e6c928a0371e98e4ed91a737092d9a6
jasmineathea <jna009@uib.no>
2024-03-20 16:20:53 +0100

    Added databases and login / logout logic

--- capp/carbon_calc/routes.py
+++ capp/carbon_calc/routes.py
@@ -1,7 +1,10 @@
 from flask import render_template, Blueprint
+from capp.models import Transport
+from flask_login import login_required
 
-carbon_calc=Blueprint('carbon_calc',__name__)
+carbon_calc = Blueprint('carbon_calc',__name__)
 
 @carbon_calc.route('/carbon_calc')
+@login_required
 def carbon_calc_home():
-    return render_template('carbon_calc/carbon_calc.html', title='carbon_calc')
+    return render_template('carbon_calc/carbon_calc.html', title = 'carbon_calc')
