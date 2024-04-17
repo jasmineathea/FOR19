e8267ee91a4e73923b94ea13cb98adca43026694
jasmineathea <jna009@uib.no>
2024-04-17 18:20:32 +0200

    Carbon emissions divided buy passengers

--- capp/carbon_calc/forms.py
+++ capp/carbon_calc/forms.py
@@ -1,61 +1,77 @@
 from flask_wtf import FlaskForm
-from wtforms import  SubmitField, SelectField, FloatField, RadioField, StringField
+from wtforms import  SubmitField, SelectField, FloatField, StringField
 from wtforms.validators import InputRequired
 
+# Bus form
 class BusForm(FlaskForm):
   kms = FloatField('Kilometers', [InputRequired()])
   fuel_type = SelectField('Type of Fuel', [InputRequired()], 
     choices=[('Diesel', 'Diesel'), ('Electric', 'Electric'), ('Hybrid', 'Hybrid')])
   submit = SubmitField('Submit')
 
+# Car form ("standard" car)
 class CarForm(FlaskForm):
   kms = FloatField('Kilometers', [InputRequired()])
-  fuel_type = SelectField('Type of Fuel', [InputRequired()], 
-    choices=[('Petrol', 'Petrol'), ('Diesel', 'Diesel'), ('Hybrid', 'Hybrid'), ('Electric', 'Electric'), ('Hydrogen', 'Hydrogen')])
+  fuel_type = SelectField('Type of fuel', [InputRequired()], 
+    choices=[('Gasoline', 'Gasoline'),
+             ('Diesel', 'Diesel'),
+             ('Hybrid', 'Hybrid'),
+             ('Electric', 'Electric'),
+             ('Hydrogen', 'Hydrogen')])
   submit = SubmitField('Submit')
 
+# Car form (connected to API)
 class SpecificCarForm(FlaskForm):
   kms = FloatField('Kilometers', [InputRequired()])
   model = StringField("Model", [InputRequired()])
   submit = SubmitField('Submit')
 
+# Plane form
 class PlaneForm(FlaskForm):
   kms = FloatField('Kilometers', [InputRequired()])
-  fuel_type = SelectField('Type of Fuel', [InputRequired()], 
-    choices=[('Short-haul(Buisness)','Short-haul(Buisness)'),
-             ('Long-haul(Economy)','Long-haul(Economy)'),
-             ('Long-haul(Premium economy)', 'Long-haul(Premium economy)'),
-             ('Long-haul(First-class)','Long-haul(First-class)'),
-             ('International(Economy)', 'International(Economy)'),
+  fuel_type = SelectField('Type of flight and class', [InputRequired()], 
+    choices=[('Short-haul (Buisness)','Short-haul (Buisness)'),
+             ('Long-haul (Economy)','Long-haul (Economy)'),
+             ('Long-haul (Premium economy)', 'Long-haul (Premium economy)'),
+             ('Long-haul (First-class)','Long-haul (First-class)'),
+             ('International (Economy)', 'International (Economy)'),
              ('International (Premium economy)','International (Premium economy)'), 
              ('International (Buisness)', 'International (Buisness)'),
              ('International (First Class)','International (First Class)')])
   submit = SubmitField('Submit')
-  
+
+# Ferry form
 class FerryForm(FlaskForm):
   kms = FloatField('Kilometers', [InputRequired()])
-  fuel_type = SelectField('Type of Fuel', [InputRequired()], 
-    choices=[('Regular', 'Regular'), ('High-speed', 'High-speed')])
+  fuel_type = SelectField('Type of ferry', [InputRequired()], 
+    choices=[('Regular', 'Regular'),
+             ('High-speed', 'High-speed')])
   submit = SubmitField('Submit')  
 
+# Train form
 class TrainForm(FlaskForm):
   kms = FloatField('Kilometers', [InputRequired()])
   fuel_type = SelectField('Type of Fuel', [InputRequired()], 
-    choices=[('Diesel', 'Diesel'), ('Electric(Nordic)', 'Electric(Nordic)'),('Electric(EU)', 'Electric(EU)')])
+    choices=[('Diesel', 'Diesel'),
+             ('Electric (Nordic)', 'Electric (Nordic)'),
+             ('Electric (EU)', 'Electric (EU)')])
   submit = SubmitField('Submit')  
 
+# Motorbike form
 class MotorbikeForm(FlaskForm):
   kms = FloatField('Kilometers', [InputRequired()])
-  fuel_type = SelectField('Type of Fuel', [InputRequired()], 
+  fuel_type = SelectField('Engine size', [InputRequired()], 
     choices=[('≤125cc', '≤125cc'), ('125cc to 500cc', '125cc to 500cc'),('>500cc','>500cc')])
   submit = SubmitField('Submit')
 
+# Bike form
 class BicycleForm(FlaskForm):
   kms = FloatField('Kilometers', [InputRequired()])
   fuel_type = SelectField('Type of Fuel', [InputRequired()], 
     choices=[('No Fossil Fuel', 'No Fossil Fuel')])
   submit = SubmitField('Submit')  
 
+# Walk form
 class WalkForm(FlaskForm):
   kms = FloatField('Kilometers', [InputRequired()])
   fuel_type = SelectField('Type of Fuel', [InputRequired()], 
--- capp/carbon_calc/routes.py
+++ capp/carbon_calc/routes.py
@@ -1,44 +1,65 @@
-from flask import render_template, Blueprint, request, redirect, url_for, flash
+from flask import (Blueprint,
+    render_template,
+    redirect, 
+    url_for,
+    flash)
+from datetime import (timedelta,
+    datetime)
+from flask_login import (login_required,
+    current_user)
+from capp.carbon_calc.forms import (BusForm,
+    CarForm,
+    PlaneForm,
+    FerryForm,
+    TrainForm,
+    MotorbikeForm,
+    BicycleForm,
+    WalkForm,
+    SpecificCarForm)
+from capp.carbon_interface.api import CarbonAPI
 from capp.models import Transport
 from capp import db
-from datetime import timedelta, datetime
-from flask_login import login_required, current_user
-from capp.carbon_calc.forms import BusForm, CarForm, PlaneForm, FerryForm, TrainForm, MotorbikeForm, BicycleForm, WalkForm, SpecificCarForm
-from capp.carbon_interface.api import CarbonAPI
 import json
-carbon_calc=Blueprint('carbon_calc',__name__)
 
+carbon_calc = Blueprint('carbon_calc',__name__)
 api = CarbonAPI()
 
-#Emissions factor per transport in kg per passemger km
+# Emissions factor per transport in kg per passemger km
 efco2={'Bus':{'Diesel':0.105,'Electric':0.03052,'Hybrid':0.054},
-    'Car':{'Petrol':0.0384,'Diesel':0.0343,'Hybrid':0.0298,'Electric':0.0106,'Hydrogen':0},
-    'Plane':{'Short-haul(Buisness)':0.147, 'Long-haul(Economy)': 0.235, 'Long-haul(Premium economy)': 0.427, 'Long-haul(First-class)': 0.558, 'International(Economy)': 0.140, 'International (Premium economy)': 0.229, 'International (Buisness)': 0.406, 'International (First Class)': 0.560},
+    'Car':{'Gasoline':0.0384,'Diesel':0.0343,'Hybrid':0.0298,'Electric':0.0106,'Hydrogen':0},
+    'Plane':{'Short-haul (Buisness)':0.147,
+             'Long-haul (Economy)': 0.235, 
+             'Long-haul (Premium economy)': 0.427, 
+             'Long-haul (First-class)': 0.558, 
+             'International (Economy)': 0.140,
+             'International (Premium economy)': 0.229,
+             'International (Buisness)': 0.406,
+             'International (First Class)': 0.560},
     'Ferry':{'Regular':0.226, 'High-speed':0.452},
     'Train':{'Diesel':0.091, 'Electric (Nordic)':0.024, 'Electric (EU)': 0.007},
     'Motorbike':{'≤125cc':0.0415,'125cc to 500cc':0.0505, '>500cc':0.0665,},
     'Bicycle':{'No Fossil Fuel':0},
     'Walk':{'No Fossil Fuel':0}}
 
-#Carbon app, main page
+# Carbon app, main page
 @carbon_calc.route('/carbon_calc')
 @login_required
 def carbon_calc_home():
     return render_template('carbon_calc/carbon_calc.html', title='carbon_calc')
 
-#New entry bus
+# New entry bus
 @carbon_calc.route('/carbon_calc/new_entry_bus', methods=['GET','POST'])
 @login_required
 def new_entry_bus():
     form = BusForm()
     if form.validate_on_submit():
         kms = form.kms.data
-        fuel = form.fuel_type.data
+        fuel = form.fuel_type.data 
         transport = 'Bus'
         # kms = request.form['kms']
         # fuel = request.form['fuel_type']
 
-        co2 = float(kms) * efco2[transport][fuel]
+        co2 = (float(kms) * efco2[transport][fuel]) / 70 # Divided by passenger(s)
 
         co2 = float("{:.2f}".format(co2))
 
@@ -48,7 +69,7 @@ def new_entry_bus():
         return redirect(url_for('carbon_calc.your_data'))
     return render_template('carbon_calc/new_entry_bus.html', title='new entry bus', form=form)
 
-#New entry car
+# New entry car
 @carbon_calc.route('/carbon_calc/new_entry_car', methods=['GET','POST'])
 @login_required
 def new_entry_car():
@@ -60,7 +81,7 @@ def new_entry_car():
         # kms = request.form['kms']
         # fuel = request.form['fuel_type']
 
-        co2 = float(kms) * efco2[transport][fuel]
+        co2 = (float(kms) * efco2[transport][fuel]) / 4
 
         co2 = float("{:.2f}".format(co2))
     
@@ -70,7 +91,7 @@ def new_entry_car():
         return redirect(url_for('carbon_calc.your_data'))
     return render_template('carbon_calc/new_entry_car.html', title='new entry car', form=form)    
 
-#New entry specific car
+# New entry specific car
 @carbon_calc.route('/carbon_calc/new_entry_specific_car', methods=['GET','POST'])
 @login_required
 def new_entry_specific_car():
@@ -84,6 +105,8 @@ def new_entry_specific_car():
         estimate = api.getEstimate(id=model, distance=kms)
         co2 = estimate["data"]["attributes"]["carbon_kg"]
         co2 = int(co2)/4
+
+        co2 = float("{:.2f}".format(co2))
     
         emissions = Transport(kms=kms, transport=transport, fuel=fuel, co2=co2, author=current_user)
         db.session.add(emissions)
@@ -92,7 +115,7 @@ def new_entry_specific_car():
     return render_template('carbon_calc/new_entry_specific_car.html', title='new entry specific car', form=form)
 
 
-#New entry plane
+# New entry plane
 @carbon_calc.route('/carbon_calc/new_entry_plane', methods=['GET','POST'])
 @login_required
 def new_entry_plane():
@@ -104,7 +127,7 @@ def new_entry_plane():
         # kms = request.form['kms']
         # fuel = request.form['fuel_type']
 
-        co2 = float(kms) * efco2[transport][fuel]
+        co2 = (float(kms) * efco2[transport][fuel]) / 100 # average amount of passengers (for all types of flights)
 
         co2 = float("{:.2f}".format(co2))
 
@@ -114,7 +137,7 @@ def new_entry_plane():
         return redirect(url_for('carbon_calc.your_data'))
     return render_template('carbon_calc/new_entry_plane.html', title='new entry plane', form=form)  
 
-#New entry ferry
+# New entry ferry
 @carbon_calc.route('/carbon_calc/new_entry_ferry', methods=['GET','POST'])
 @login_required
 def new_entry_ferry():
@@ -126,7 +149,7 @@ def new_entry_ferry():
         # kms = request.form['kms']
         # fuel = request.form['fuel_type']
 
-        co2 = float(kms) * efco2[transport][fuel]
+        co2 = (float(kms) * efco2[transport][fuel]) / 309
 
         co2 = float("{:.2f}".format(co2))
 
@@ -136,7 +159,7 @@ def new_entry_ferry():
         return redirect(url_for('carbon_calc.your_data'))
     return render_template('carbon_calc/new_entry_ferry.html', title='new entry ferry', form=form)     
 
-#New entry motorbike
+# New entry motorbike
 @carbon_calc.route('/carbon_calc/new_entry_motorbike', methods=['GET','POST'])
 @login_required
 def new_entry_motorbike():
@@ -179,7 +202,7 @@ def new_entry_train():
         return redirect(url_for('carbon_calc.your_data'))
     return render_template('carbon_calc/new_entry_train.html', title='new entry train', form=form) 
 
-#New entry bicycle
+# New entry bicycle
 @carbon_calc.route('/carbon_calc/new_entry_bicycle', methods=['GET','POST'])
 @login_required
 def new_entry_bicycle():
@@ -201,7 +224,7 @@ def new_entry_bicycle():
         return redirect(url_for('carbon_calc.your_data'))
     return render_template('carbon_calc/new_entry_bicycle.html', title='new entry bicycle', form=form)
 
-#New entry walk
+# New entry walk
 @carbon_calc.route('/carbon_calc/new_entry_walk', methods=['GET','POST'])
 @login_required
 def new_entry_walk():
@@ -223,16 +246,16 @@ def new_entry_walk():
         return redirect(url_for('carbon_calc.your_data'))
     return render_template('carbon_calc/new_entry_walk.html', title='new entry walk', form=form)
 
-#Your data
+# Your data
 @carbon_calc.route('/carbon_calc/your_data')
 @login_required
 def your_data():
-    #Table
+    # Table
     entries = Transport.query.filter_by(author=current_user). \
         filter(Transport.date> (datetime.now() - timedelta(days=5))).\
         order_by(Transport.date.desc()).order_by(Transport.transport.asc()).all()
     
-    #Emissions by category
+    # Emissions by category
     emissions_by_transport = db.session.query(db.func.sum(Transport.co2), Transport.transport). \
         filter(Transport.date > (datetime.now() - timedelta(days=5))).filter_by(author=current_user). \
         group_by(Transport.transport).order_by(Transport.transport.asc()).all()
@@ -285,7 +308,7 @@ def your_data():
     else:
         emission_transport[6]
 
-    #Kilometers by category
+    # Kilometers by category
     kms_by_transport = db.session.query(db.func.sum(Transport.kms), Transport.transport). \
         filter(Transport.date > (datetime.now() - timedelta(days=5))).filter_by(author=current_user). \
         group_by(Transport.transport).order_by(Transport.transport.asc()).all()
@@ -350,7 +373,7 @@ def your_data():
     else:
         kms_transport[8]    
 
-    #Emissions by date (individual)
+    # Emissions by date (individual)
     emissions_by_date = db.session.query(db.func.sum(Transport.co2), Transport.date). \
         filter(Transport.date > (datetime.now() - timedelta(days=5))).filter_by(author=current_user). \
         group_by(Transport.date).order_by(Transport.date.asc()).all()
@@ -360,7 +383,7 @@ def your_data():
         dates_label.append(date.strftime("%m-%d-%y"))
         over_time_emissions.append(total)    
 
-    #Kms by date (individual)
+    # Kms by date (individual)
     kms_by_date = db.session.query(db.func.sum(Transport.kms), Transport.date). \
         filter(Transport.date > (datetime.now() - timedelta(days=5))).filter_by(author=current_user). \
         group_by(Transport.date).order_by(Transport.date.asc()).all()
@@ -369,7 +392,6 @@ def your_data():
     for total, date in kms_by_date:
         dates_label.append(date.strftime("%m-%d-%y"))
         over_time_kms.append(total)      
-
 
     return render_template('carbon_calc/your_data.html', title='your_data', entries=entries,
         emissions_by_transport_python_dic=emissions_by_transport,     
@@ -380,7 +402,7 @@ def your_data():
         over_time_kms=json.dumps(over_time_kms),
         dates_label=json.dumps(dates_label))
 
-#Delete emission
+# Delete emission
 @carbon_calc.route('/carbon_calc/delete-emission/<int:entry_id>')
 def delete_emission(entry_id):
     entry = Transport.query.get_or_404(int(entry_id))
@@ -389,4 +411,3 @@ def delete_emission(entry_id):
     flash("Entry deleted", "success")
     return redirect(url_for('carbon_calc.your_data'))
     
-# todo new entry train
